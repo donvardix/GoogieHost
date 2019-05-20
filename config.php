697ef97079db3596a7ce68913a5e1d65aa50fdbe
donvardix <donvardix@gmail.com>
2019-05-20 19:46:44 +0300

    config

--- config.php
+++ config.php
@@ -6,4 +6,7 @@ $user='root';
 $password='fghtkm96';
 $pdo=new PDO('mysql:host='.$host.';dbname='.$dbname, $user, $password);
 
+// /home/donvardp/public_html/
+// /usr/local/bin/php /home/donvardp/public_html/update.php
+
 ?>
