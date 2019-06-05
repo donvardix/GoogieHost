edd166d2108274b940c7ef79bf895e87375021ed
donvardix <donvardix@gmail.com>
2019-06-05 07:16:36 +0300

    Новый код - суммирует продажы по датам

--- Controllers/New.php
+++ Controllers/New.php
@@ -37,6 +37,55 @@ class New
     echo $sum;
 
   }
+  // (LARAVEL не обязателен)
+  // Количесво продаж предмета на ТП Steam по датам
+  public static function curl(){
+    $url="https://steamcommunity.com/market/listings/570/Bracers%20of%20the%20Cavern%20Luminar";
+    $init=curl_init($url); // Инициализирует сеанс cURL
+    curl_setopt($init, CURLOPT_FOLLOWLOCATION, true); // Разрешает редирект по сайту
+    curl_setopt($init, CURLOPT_RETURNTRANSFER, true); // Возврат результата в качестве строки
+    $html=curl_exec($init); // Помещаем HTML в переменную
+    curl_close($init);  // Закрываем сеанс
+
+    // Парсим данные с графика steam
+    $start = 'var line1=';
+    $end = ']];';
+    $delete_before=substr($html, strpos($html, $start)+strlen($start));
+    $data=substr($delete_before, 0, strpos($delete_before, $end)+strlen($end)-1);
+
+    // Полученную строку преобразуем в массив
+    $array=json_decode($data);
+    //$array=array_slice($array, -200);
+
+    // В текущем массиве изменяем формат датны 'Jun 05 2019 02: +0' => '05.06.2019'
+    foreach($array as $key => &$r){
+      $date=$r[0];
+      $r[0]=date('d.m.Y', strtotime(substr($date, 0, strpos($date, ":")-3)));
+    }
+
+    // Создает новый массив $new и суммирует продажы по датам
+    $new = [];
+    $l=count($array);
+    for($i=0;$i<$l;$i++){
+        $index = -1;
+        $l2=count($new);
+        for($j=0;$j<$l2;$j++){
+            if($array[$i][0] == $new[$j][0]){
+                $index = $j;
+                break;
+            }
+        }
+        if($index == -1){
+            array_push($new, $array[$i]);
+        }else{
+            $new[$index][2] += $array[$i][2];
+        }
+    }
+
+    $new=array_slice($new, -200); // Получаем последние 200 записей в массиве
+    //dump($new); 
+  }
+
 }
 
 
